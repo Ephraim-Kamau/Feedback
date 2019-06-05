0cd75526fa4fa4c9fcffcdfed4e7eceeff8ded45
Ephraim-Kamau <kephraim19@gmail.com>
2019-06-05 13:37:36 +0300

    Add search bar

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.projects_today,name='projectsToday'),
-    
+    url(r'^search/', views.search_results, name='search_results'),    
 ]
--- projects/views.py
+++ projects/views.py
@@ -8,4 +8,15 @@ def projects_today(request):
 
     return render(request, 'today-projects.html', {"date":date,})    
 
-   
+def search_results(request):
+
+    if 'topics' in request.GET and request.GET["topics"]:
+        search_term = request.GET.get("topics")
+        searched_articles = Topics.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"topics": searched_topics})
+
+    else:
+        message = "You haven't searched for any topic"
+        return render(request, 'search.html',{"message":message})   
