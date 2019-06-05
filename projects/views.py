21a23f4d2995b43d239e30b6bcd2a743cfd7f620
Ephraim-Kamau <kephraim19@gmail.com>
2019-06-05 12:57:36 +0300

    Add changes to project structure

--- projects/views.py
+++ projects/views.py
@@ -3,12 +3,9 @@ from django.shortcuts import render
 import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return render( request, 'welcome.html')
-
 def projects_today(request):
     date = dt.date.today()
 
-    return render(request, 'all-projects/today-projects.html', {"date":date,})    
+    return render(request, 'today-projects.html', {"date":date,})    
 
    
